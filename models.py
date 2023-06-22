69d9e6daec83f0321a4ea9821c15cce1fc6f210d
Rick Sugden <rickjsugden@gmail.com>
2023-06-22 12:19:00 -0400

    added DeepConvNet and adapted training loop

--- models.py
+++ models.py
@@ -314,3 +314,63 @@ class VGG13(nn.Module):
         
         x = F.softmax(x, dim=1)
         return x
+    
+
+class DeepConvNet(torch.nn.Module):
+    def __init__(self, n_output):
+        super(DeepConvNet, self).__init__()
+        self.block1 = nn.Sequential(
+            # Conv2d(1, 25, kernel_size=(1,5),padding='VALID',bias=False),
+            # Conv2d(25, 25, kernel_size=(2,1), padding='VALID',bias=False),
+            nn.Conv2d(1, 25, kernel_size=(1,5),bias=False),
+            nn.Conv2d(25, 25, kernel_size=(2,1),bias=False),
+            nn.BatchNorm2d(25, eps=1e-05, momentum=0.1),
+            nn.LeakyReLU(negative_slope=0.04),
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.47)
+        )
+
+        self.block2 = nn.Sequential(
+            # Conv2d(25, 50, kernel_size=(1,5),padding='VALID',bias=False),
+            nn.Conv2d(25, 50, kernel_size=(1,5),bias=False),
+            nn.BatchNorm2d(50, eps=1e-05, momentum=0.1),
+            nn.LeakyReLU(negative_slope=0.09),
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.47),
+        )
+        self.block3 = nn.Sequential(
+            # Conv2d(50, 100, kernel_size=(1,5),padding='VALID',bias=False),
+            nn.Conv2d(50, 100, kernel_size=(1,5),bias=False),
+            nn.BatchNorm2d(100, eps=1e-05, momentum=0.1),
+            nn.LeakyReLU(negative_slope=0.04),
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.47),
+        )
+        self.block4 = nn.Sequential(
+            # Conv2d(100, 200, kernel_size=(1,5),padding='VALID',bias=False),
+            nn.Conv2d(100, 200, kernel_size=(1,5),bias=False),
+            nn.BatchNorm2d(200, eps=1e-05, momentum=0.1),
+            nn.LeakyReLU(negative_slope=0.09),
+            nn.MaxPool2d(kernel_size=(1,2)),
+            nn.Dropout(p=0.47),
+        )
+        self.mlp = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(1793600,n_output,bias=True),
+            nn.Softmax(dim=1)
+        )
+
+    def forward(self, x):
+        x = x.view(x.shape[0], 1, x.shape[1], x.shape[2])
+        x = self.block1(x)
+        #print(x.shape)
+        x = self.block2(x)
+        #print(x.shape)
+        x = self.block3(x)
+        #print(x.shape)
+        x = self.block4(x)
+        #print(x.shape)
+        x = self.mlp(x)
+        #print(x.shape)
+        return x
+       
